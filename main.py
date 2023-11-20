b09d2b2df7daac98a83daf73ea7fd1db90417f16
Dallas <dallashansen.work@gmail.com>
2023-11-20 13:31:48 -0500

    added class 'Contestant'

---
+++ main.py
@@ -0,0 +1,15 @@
+class Contestant:
+    def __init__(self, name):
+        self.name = name
+        self.money = 0
+    
+    def add_money(self, amount):
+        self.money += amount
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
